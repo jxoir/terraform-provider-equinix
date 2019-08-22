6ac2930ae5cddd704bd154be565163a0eca1e105
Christian Melendez <christianhxc@gmail.com>
2019-08-22 16:24:52 +0200

    Added the AWS provisioning example

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # terraform-provider-equinix
 Terraform Equinix Provider
 =======
+
+## Compile Locally
+go build -o .\examples\terraform-provider-equinix.exe
--- examples/main.tf
+++ examples/main.tf
@@ -2,6 +2,15 @@ provider "equinix" {
     debug = true
 }
 
-resource "equinix_ecx_connection" "jxo" {
-	uuid = "e235ab08-09ef-40c7-a926-5943df612046"
+resource "equinix_ecx_connection" "aws" {
+    primary_name = "TERRAFORM_AWS_TEST"
+    speed = 50
+    speed_unit = "MB"
+    seller_profile_uuid = "69ee618d-be52-468d-bc99-00566f2dd2b9" // AWS
+    primary_port_uuid = "66284add-49a3-9a30-b4e0-30ac094f8af1" // AWS DX "Standard"
+    authorization_key = "123456789012" // AWS Account Number
+    notifications = ["some@email.com"]
+    primary_vlan_stag = 3020
+    seller_metro_code = "LD"
+    seller_region = "eu-west-1"
 }
